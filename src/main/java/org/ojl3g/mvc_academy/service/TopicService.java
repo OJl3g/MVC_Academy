024152a75d92c5e620f19c379b2286811d990ae7
OJl3g <oleg.tiabin@gmail.com>
2024-09-29 12:24:22 +0300

    create : GroupRepository, TopicRepository, GroupService, TopicService, GroupServiceImpl, TopicServiceImpl.

--- src/main/java/org/ojl3g/mvc_academy/service/TopicService.java
+++ src/main/java/org/ojl3g/mvc_academy/service/TopicService.java
@@ -1,13 +1,19 @@
 package org.ojl3g.mvc_academy.service;
 
 import org.ojl3g.mvc_academy.model.Topic;
+
 import java.util.List;
 
 public interface TopicService {
     Topic saveTopic(Topic topic);
+
     Topic getTopicById(Long id);
+
     List<Topic> getAllTopics();
+
     void deleteTopic(Long id);
+
     Topic updateTopic(Topic topic);
+
     List<Topic> getTopicByName(String name);
 }
